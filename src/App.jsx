be9ada3640a687a69ee546d594ada30c4ff19337
Kyrylo Haiduk <kyrylo@talentpath.co>
2024-09-19 11:19:05 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,83 @@
-import React from 'react';
+/* eslint-disable function-paren-newline */
+import React, { useState } from 'react';
+import cn from 'classnames';
 
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
-// import peopleFromServer from './people.json';
+import peopleFromServer from './people.json';
+
+const COLUMNS = ['name', 'sex', 'born', 'selection'];
 
 export function App() {
+  const [selectedPeople, setSelectedPeople] = useState([]);
+
+  const handleAddPerson = person => {
+    setSelectedPeople(people => [...people, person]);
+  };
+
+  const handleRemovePerson = person =>
+    setSelectedPeople(people =>
+      people.filter(selectedPerson => selectedPerson !== person),
+    );
+
   return (
     <div className="box">
       <h1 className="title">People table</h1>
 
       <table className="table is-striped is-narrow">
+        <caption>
+          {selectedPeople.map(person => person.name).join(', ')}
+        </caption>
+
         <thead>
           <tr>
-            <th>name</th>
-            <th>sex</th>
-            <th>born</th>
+            {COLUMNS.map(column => (
+              <th key={column}>{column}</th>
+            ))}
           </tr>
         </thead>
 
         <tbody>
-          <tr>
-            <td>Carolus Haverbeke</td>
-            <td>m</td>
-            <td>1832</td>
-          </tr>
+          {peopleFromServer.map(person => {
+            const isSelected = selectedPeople.includes(person);
 
-          <tr>
-            <td>Emma de Milliano</td>
-            <td>f</td>
-            <td>1842</td>
-          </tr>
+            return (
+              <tr
+                key={person.slug}
+                className={cn({
+                  'has-background-link': person.sex === 'm' && !isSelected,
+                  'has-background-danger': person.sex === 'f' && !isSelected,
+                  'has-background-warning': isSelected,
+                })}
+              >
+                <td>{person.name}</td>
+                <td>{person.sex}</td>
+                <td>{person.born}</td>
+
+                <td>
+                  {isSelected ? (
+                    <button
+                      type="button"
+                      onClick={() => handleRemovePerson(person)}
+                      className="button has-background-danger"
+                    >
+                      <i className="fas fa-minus" />
+                    </button>
+                  ) : (
+                    <button
+                      type="button"
+                      onClick={() => handleAddPerson(person)}
+                      className="button"
+                    >
+                      <i className="fas fa-plus" />
+                    </button>
+                  )}
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
